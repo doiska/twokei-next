3b8adb45c98614f515b70e305af135d6e087f837
doiská <doiskka@gmail.com>
2022-11-23 15:54:01 -0300

    (feat) stop song command

---
+++ src/commands/stop.ts
@@ -0,0 +1,25 @@
+import { CommandContext } from "../handlers/command/command.types";
+import { registerCommand } from "../handlers/command/CommandRegister";
+
+const stopCommand = async (context: CommandContext) => {
+  const { guild } = context;
+
+  if (!guild) {
+    return;
+  }
+
+  const player = context.client.music.getPlayer(guild.id);
+
+  if (!player) {
+    return;
+  }
+
+  await player.stop();
+
+  return `Stopped playing`;
+}
+
+registerCommand({
+  name: 'stop',
+  description: 'Stop the current song',
+}, stopCommand);
