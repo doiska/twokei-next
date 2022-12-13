473179197a3b94b019d454263bb78cdf81be7985
doiská <doiskka@gmail.com>
2022-12-13 11:17:26 -0300

    (refactor) removed old command/event system

--- src/handlers/events/EventRegister.ts
+++
@@ -1,8 +0,0 @@
-import { ClientEvents, Events } from 'discord.js'
-import { Twokei } from "../../app/Twokei";
-import { Player } from "shoukaku";
-import { PlayerEventType } from "shoukaku/dist/src/guild/Player";
-
-export const registerEvent = <T extends keyof ClientEvents>(name: T, handler: (...args: ClientEvents[T]) => void) => {
-  Twokei.on(name, handler);
-};
