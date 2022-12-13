c21dd1aa5e043f923686406ed5ccf160f70081be
doiská <doiskka@gmail.com>
2022-12-13 11:17:48 -0300

    (feat) added Twokei Framework and extended TwokeiClient

--- src/structures/ExtendedClient.ts
+++ src/structures/ExtendedClient.ts
@@ -1,86 +1,76 @@
-import { Client, ClientOptions, REST, Routes } from "discord.js";
+import { ClientOptions } from "discord.js";
 import { Connectors, Shoukaku } from "shoukaku";
 import { ClusterClient as ShardClient, DjsClient } from "discord-hybrid-sharding";
 import { Nodes, shoukakuOptions } from "../shoukaku/options";
-import glob from "fast-glob";
-import { Command } from "../handlers/command/command.types";
-import { parseCommandToSlashJSON } from "../handlers/command/CommandRegister";
 import { Twokei } from "../app/Twokei";
 import { logger } from "../utils/Logger";
 import { Xiao } from "../xiao/Xiao";
 
-export class ExtendedClient extends Client {
+import { TwokeiClient } from "twokei-framework";
 
-  public shoukaku: Shoukaku;
-  public cluster: ShardClient;
-  public xiao: Xiao;
+declare module "discord.js" {
+	interface Client {
+		shoukaku: Shoukaku;
+		cluster: ShardClient;
+		xiao: Xiao;
+	}
+}
 
-  public commands: Map<string, Command> = new Map();
+export class ExtendedClient extends TwokeiClient {
 
-  private _paths = ["../events/**/*.{js,ts}", "../commands/**/*.{js,ts}"];
+	public shoukaku: Shoukaku;
+	public cluster: ShardClient;
+	public xiao: Xiao;
 
-  private _exiting = false;
+	private _exiting = false;
 
-  constructor(options: ClientOptions) {
-    super(options);
+	constructor(options: ClientOptions) {
+		super({
+			...options,
+			currentWorkingDirectory: __dirname,
+			commandsPath: `../commands/**/*.{ts,js}`,
+			// eventsPath: `../events/**/*.{ts,js}`,
+			autoload: true
+		});
 
-    this.shoukaku = new Shoukaku(new Connectors.DiscordJS(this), Nodes, shoukakuOptions);
+		this.shoukaku = new Shoukaku(new Connectors.DiscordJS(this), Nodes, shoukakuOptions);
 
-    this.xiao = new Xiao({
-      send: (guildId, payload) => {
-        const guild = Twokei.guilds.cache.get(guildId);
-        if (guild) guild.shard.send(payload);
-      },
-      defaultSearchEngine: "youtube",
-    }, new Connectors.DiscordJS(this), Nodes, shoukakuOptions);
+		this.xiao = new Xiao({
+			send: (guildId, payload) => {
+				const guild = Twokei.guilds.cache.get(guildId);
+				if (guild) guild.shard.send(payload);
+			},
+			defaultSearchEngine: "youtube",
+		}, new Connectors.DiscordJS(this), Nodes, shoukakuOptions);
 
-    this.cluster = new ShardClient(this as unknown as DjsClient);
+		this.cluster = new ShardClient(this as unknown as DjsClient);
 
-    this.shoukaku.on('ready', (name, info) => logger.info(`Shoukaku -> ${name} ${info}`));
-    this.shoukaku.on('error', (name, info) => logger.error(`Shoukaku -> ${name} ${info}`));
-    // this.shoukaku.on('debug', (name, info) => logger.verbose(`Shoukaku -> ${name} ${info}`));
+		// this.shoukaku.on('ready', (name, info) => logger.info(`Shoukaku -> ${name} ${info}`));
+		// this.shoukaku.on('error', (name, info) => logger.error(`Shoukaku -> ${name} ${info}`));
+		// this.shoukaku.on('debug', (name, info) => logger.verbose(`Shoukaku -> ${name} ${info}`));
 
-    ['beforeExit', 'SIGUSR1', 'SIGUSR2', 'SIGINT', 'SIGTERM'].forEach(event => process.once(event, this.exit.bind(this)));
-  }
+		['beforeExit', 'SIGUSR1', 'SIGUSR2', 'SIGINT', 'SIGTERM'].forEach(event => process.once(event, this.exit.bind(this)));
+		// this.eventHandler.loadEvents();
+	}
 
-  public async start(): Promise<void> {
-    await this.register();
-    await this.registerLoggers();
-    await this.registerSlashCommands();
-    await this.login(process.env.TOKEN);
-  }
+	public async start(): Promise<void> {
+		await this.registerLoggers();
+		await this.login(process.env.TOKEN);
+	}
 
-  private async register(): Promise<void> {
-    const files = await glob(this._paths, { cwd: __dirname, onlyFiles: true });
-    files.forEach((file) => import(file));
-  }
+	private async registerLoggers(): Promise<void> {
+		process.on('uncaughtException', (err) => logger.error(`Uncaught Exception: ${err.stack}`));
+		process.on('unhandledRejection', (reason: string, err) => logger.error(`Unhandled Rejection: ${reason}`, err));
+	}
 
-  private async registerSlashCommands() {
+	private exit() {
+		if (this._exiting) {
+			return;
+		}
 
-    if (!process.env.CLIENT_ID || typeof Twokei.token !== "string") {
-      return;
-    }
+		this._exiting = true;
 
-    const parsed = Array.from(this.commands.values()).map(parseCommandToSlashJSON);
-
-    await new REST({ version: "10" })
-      .setToken(Twokei.token)
-      .put(Routes.applicationGuildCommands(process.env.CLIENT_ID, "926643164201234533"), { body: parsed });
-  }
-
-  private async registerLoggers(): Promise<void> {
-    process.on('uncaughtException', (err) => logger.error(`Uncaught Exception: ${err.stack}`));
-    process.on('unhandledRejection', (reason: string, err) => logger.error(`Unhandled Rejection: ${reason}`, err));
-  }
-
-  private exit() {
-    if (this._exiting) {
-      return;
-    }
-
-    this._exiting = true;
-
-    this.destroy();
-    process.exit(0);
-  }
+		this.destroy();
+		process.exit(0);
+	}
 }
