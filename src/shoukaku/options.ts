34f4e83cbfcb1fa61c36c3293ad97951a702bcf2
doiská <doiskka@gmail.com>
2022-12-13 11:18:06 -0300

    (feat) added active option to Lava nodes

--- src/shoukaku/options.ts
+++ src/shoukaku/options.ts
@@ -1,19 +1,23 @@
 import { NodeOption, ShoukakuOptions } from "shoukaku";
 
-export const Nodes: NodeOption[] = [
+type OptionWithActive = NodeOption & { active?: boolean };
+
+export const Nodes: OptionWithActive[] = [
   {
     name: "freelava.ga",
     url: 'node1.kartadharta.xyz:443',
     secure: true,
     auth: "kdlavalink",
+    active: true
   },
   {
     name: "lavalink.oops.wtf",
     url: 'lavalink.oops.wtf',
     secure: true,
-    auth: "www.freelavalink.ga"
+    auth: "www.freelavalink.ga",
+    active: false
   }
-];
+].filter(i => i.active);
 
 export const shoukakuOptions: ShoukakuOptions = {
   resume: true,
