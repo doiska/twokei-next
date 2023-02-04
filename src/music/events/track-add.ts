e5edf8107a574b5bccea3e778b5eb3be92f0b647
doiská <doiskka@gmail.com>
2023-02-04 00:30:24 -0300

    (fix) removed redundant refresh from track-start components

--- src/music/events/track-add.ts
+++ src/music/events/track-add.ts
@@ -3,8 +3,6 @@ import { XiaoEvents } from '../controllers/Xiao';
 import { Twokei } from '../../app/Twokei';
 
 export const trackAdd: XiaoEvents[Events.TrackAdd] = (venti) => {
-  console.log(`[Xiao] Track added to queue. Refreshing components...`);
-
   Twokei.xiao.embedManager.get(venti.guildId)?.from(venti)
     .refreshEmbed()
     .refreshComponents()
