bd95ac03e872081fc8ff58f1bb55436679aaab4d
doiská <doiskka@gmail.com>
2023-02-21 13:16:44 -0300

    (feat) added playlist buttons (without use yet)

--- src/constants/music.ts
+++ src/constants/music.ts
@@ -1,10 +1,13 @@
-import { Awaitable, ButtonStyle, GuildMember } from 'discord.js';
+import { ButtonStyle, GuildMember } from 'discord.js';
 import { skipSong } from '../music/heizou/skip-song';
 import { setLoopState } from '../music/heizou/set-loop-state';
 import { shuffleQueue } from '../music/heizou/shuffle-queue';
 import { previousSong } from '../music/heizou/previous-song';
 import { destroyPlayerInstance } from '../music/heizou/destroy-player-instance';
 import { pauseSong } from '../music/heizou/pause-song';
+import { Venti } from '../music/controllers/Venti';
+import { getFixedT } from 'i18next';
+import { Locale } from '../translation/i18n';
 
 export enum Menus {
   SelectSongMenu = 'SELECT_SONG_MENU',
@@ -13,67 +16,118 @@ export enum Menus {
 export type Button = {
   execute: (userOrGuildId: GuildMember) => Promise<unknown>
   emoji?: string;
-  style?: ButtonStyle.Primary | ButtonStyle.Secondary | ButtonStyle.Success | ButtonStyle.Danger
+  style?: ButtonStyle;
+  disabled?: boolean;
+  label?: string;
+  url?: string;
 }
 
-type ButtonRow = Record<'default' | 'player_primary' | 'player_secondary', Button[]>
+export const DynamicDefaultButtons = (locale?: Locale): Record<string, Button> => {
+  const t = locale ? getFixedT(locale, 'player') : () => undefined;
 
-export const DefaultButtons: Record<string, Button> = {
-  SELECT_LANGUAGE: {
-    emoji: '🌐',
-    style: ButtonStyle.Secondary,
-    execute: async (resolvable) => {
+  return {
+    SELECT_LANGUAGE: {
+      label: t('embed.buttons.select_language') || 'Select Language',
+      emoji: '🌐',
+      style: ButtonStyle.Secondary,
+      execute: async () => {
+      }
+    },
+    DONATE: {
+      label: t('embed.buttons.donate') || 'Donate',
+      emoji: '<:pray:1077449609447751791>',
+      style: ButtonStyle.Link,
+      url: 'https://www.paypal.com/paypalme/doiska',
+      execute: async () => {
 
+      }
     }
-  },
-  // SYNC_PLAYLIST: {
-  //   emoji: '🔄',
-  //   style: ButtonStyle.Success,
-  //   execute: async (resolvable) => {
-  //
-  //   }
-  // }
-}
-
-export const PlayerPrimaryButtons: Record<string, Button> = {
-  STOP: {
-    emoji: '⏹️',
-    style: ButtonStyle.Danger,
-    execute: destroyPlayerInstance
-  },
-  PREVIOUS: {
-    emoji: '⏮️',
-    style: ButtonStyle.Secondary,
-    execute: previousSong
-  },
-  PAUSE: {
-    emoji: '⏸️',
-    style: ButtonStyle.Secondary,
-    execute: pauseSong
-  },
-  SKIP: {
-    emoji: '⏭️',
-    style: ButtonStyle.Secondary,
-    execute: skipSong
   }
 }
 
-export const SecondaryButtons: Record<string, Button> = {
-  SHUFFLE: {
-    emoji: '🔀',
-    style: ButtonStyle.Secondary,
-    execute: shuffleQueue
-  },
-  LOOP: {
-    emoji: '🔁',
-    style: ButtonStyle.Secondary,
-    execute: setLoopState
-  },
-  // AUTO_PLAY: {
-  //   emoji: '🔂',
-  //   style: ButtonStyle.Secondary,
-  //   execute: async (resolvable) => {
-  //
-  //   }
-  // }
+export const DynamicPlaylistButtons = (locale?: Locale): Record<string, Button> => {
+  const t = locale ? getFixedT(locale, 'player') : () => undefined;
+
+  return {
+    LOAD_PLAYLIST: {
+      label: t('embed.buttons.your_playlists') || 'View playlists',
+      emoji: ':playlist_icon:1077444078234521700',
+      style: ButtonStyle.Secondary,
+      execute: async () => {
+
+      }
+    },
+    // SAVE_THIS_PLAYLIST: {
+    //   label: t('embed.buttons.save_this_playlist') || 'Sync Playlist',
+    //   emoji: ':spotify_dark:1077441343456018463',
+    //   style: ButtonStyle.Success,
+    //   execute: async () => {
+    //
+    //   }
+    // },
+    SYNC_PLAYLIST: {
+      label: t('embed.buttons.sync_playlist') || 'Sync Playlist',
+      emoji: ':spotify_dark:1077441343456018463',
+      style: ButtonStyle.Success,
+      execute: async () => {
+
+      }
+    }
+  }
+}
+
+export const DynamicPrimaryButtons = (player?: Venti): Record<string, Button> => {
+
+  const t = getFixedT(player?.locale ?? 'en_us', 'player');
+
+  return {
+    STOP: {
+      emoji: '⏹️',
+      style: ButtonStyle.Secondary,
+      execute: destroyPlayerInstance
+    },
+    PREVIOUS: {
+      emoji: '⏮️',
+      style: ButtonStyle.Secondary,
+      disabled: !player?.queue.previous,
+      execute: previousSong
+    },
+    PAUSE: {
+      label: t(player?.paused ? 'embed.buttons.resume' : 'embed.buttons.pause') || 'Pause',
+      emoji: '⏸️',
+      style: ButtonStyle.Secondary,
+      execute: pauseSong
+    },
+    SKIP: {
+      emoji: '⏭️',
+      style: ButtonStyle.Secondary,
+      disabled: !player?.queue.totalSize,
+      execute: skipSong
+    }
+  }
+}
+
+export const DynamicSecondaryButtons = (player?: Venti): Record<string, Button> => {
+  const t = getFixedT(player?.locale ?? 'en_us', 'player');
+
+  return {
+    SHUFFLE: {
+      emoji: '🔀',
+      style: ButtonStyle.Secondary,
+      execute: shuffleQueue
+    },
+    LOOP: {
+      label: t(`embed.buttons.loop_${player?.loop.toLowerCase()}`) || 'Loop',
+      emoji: '🔁',
+      style: player?.loop === 'none' ? ButtonStyle.Secondary : ButtonStyle.Primary,
+      execute: setLoopState
+    }
+    // AUTO_PLAY: {
+    //   emoji: '🔂',
+    //   style: ButtonStyle.Secondary,
+    //   execute: async (resolvable) => {
+    //
+    //   }
+    // }
+  }
 }
