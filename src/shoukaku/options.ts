a850bf62cd7d47e391144eb1fc03456454aa136b
doiská <doiskka@gmail.com>
2023-05-07 15:03:30 -0300

    Update nodes

--- src/shoukaku/options.ts
+++ src/shoukaku/options.ts
@@ -4,30 +4,96 @@ type OptionWithActive = NodeOption & { active?: boolean };
 
 export const Nodes: OptionWithActive[] = [
   {
-    name: 'lava1.horizxon.studio:80',
-    url: 'lava1.horizxon.studio',
-    secure: false,
-    auth: 'horizxon.studio'
+    "name": "https://open.spotify.com/track/4TuNI3WEMyLQAKRMJmcQdA",
+    "url": "narco.buses.rocks:2269",
+    "auth": "glasshost1984",
+    "secure": false
   },
   {
-    name: 'lava3.horizxon.studio:80',
-    url: 'lava3.horizxon.studio',
-    secure: false,
-    auth: 'horizxon.studio'
+    "name": "https://www.alexanderof.xyz",
+    "url": "lavalink1.albinhakanson.se:1141",
+    "auth": "albinhakanson.se",
+    "secure": false
   },
   {
-    name: 'lava4.horizxon.studio:80',
-    url: 'lava4.horizxon.studio',
-    secure: false,
-    auth: 'horizxon.studio'
+    "name": "https://sneakynodes.com/discord",
+    "url": "lavalink.sneakynodes.com:2333",
+    "auth": "sneakynodes.com",
+    "secure": false
   },
   {
-    name: "narco.buses.rocks:2269",
-    url: "narco.buses.rocks",
-    secure: true,
-    auth: "glasshost1984",
+    "name": "https://discord.gg/ZNKNY3RpRg",
+    "url": "server.alfari.id:6969",
+    "auth": "youshallnotpass",
+    "secure": false
   },
-];
+  {
+    "name": "https://discord.gg/7G9TvNvZVs",
+    "url": "my.tofumc.pro:25570",
+    "auth": "sussy",
+    "secure": false
+  },
+  {
+    "name": "https://github.com/Dep0s1t",
+    "url": "lavalink4africa.islantay.tk:8880",
+    "auth": "AmeliaWatsonisTheBest**!",
+    "secure": false
+  },
+  {
+    "name": "https://horizxon.studio/",
+    "url": "lava1.horizxon.studio:80",
+    "auth": "horizxon.studio",
+    "secure": false
+  },
+  {
+    "name": "https://horizxon.studio/",
+    "url": "lava3.horizxon.studio:80",
+    "auth": "horizxon.studio",
+    "secure": false
+  },
+  {
+    "name": "https://horizxon.studio/",
+    "url": "lava4.horizxon.studio:80",
+    "auth": "horizxon.studio",
+    "secure": false
+  },
+  {
+    "name": "https://timelessnesses.me",
+    "url": "krn.2d.gay:80",
+    "auth": "AWP)JQ$Gv9}dm.u",
+    "secure": false
+  },
+  {
+    "name": "https://mune.moe",
+    "url": "purr.aikomechawaii.live:10415",
+    "auth": "AnythingAsPassword",
+    "secure": false
+  },
+  {
+    "name": "https://aikomechawaii.live",
+    "url": "purr.aikomechawaii.live:10357",
+    "auth": "ImayBeStupid",
+    "secure": false
+  },
+  {
+    "name": "https://github.com/MiracleCherryPie",
+    "url": "lavalink.oxygen-project.cloud:2100",
+    "auth": "oxygen-project.cloud",
+    "secure": false
+  },
+  {
+    "name": "https://putraa.my.id",
+    "url": "sg1.clevent.my.id:2333",
+    "auth": "youshallnotpass",
+    "secure": false
+  },
+  {
+    "name": "https://velleri.us",
+    "url": "lv.velleri.us:2333",
+    "auth": "ilovemonkeys",
+    "secure": false
+  }
+]
 
 export const shoukakuOptions: ShoukakuOptions = {
   resume: true,
