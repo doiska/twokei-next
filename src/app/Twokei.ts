23e6006f9ff6a34f7e9df7bb3c8a9fe7ba0bb94f
doiská <doiskka@gmail.com>
2023-05-27 01:04:33 -0300

    (refactor) moved from typeorm to drizzle

--- src/app/Twokei.ts
+++ src/app/Twokei.ts
@@ -1,11 +1,8 @@
 import 'reflect-metadata';
-
 import { ExtendedClient } from '../structures/ExtendedClient';
 import { logger } from '../modules/logger-transport';
 
 export const Twokei = new ExtendedClient({
-  shards: 'auto',
-  shardCount: 1,
   intents: [
     'Guilds',
     'GuildVoiceStates',
