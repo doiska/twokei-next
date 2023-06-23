2baebe7d6e8ecf8db6228e6e5a30af23ebd0487c
doiská <doiskka@gmail.com>
2023-06-23 19:19:14 -0300

    feat(i18n): improved portuguese translation and player embed

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,14 +1,16 @@
-import i18next, { Resource, ResourceLanguage } from 'i18next';
 import * as fs from 'fs';
+import i18next, { Resource, ResourceLanguage } from 'i18next';
 import * as path from 'path';
+
 import { logger } from '../modules/logger-transport';
+import { getCommand, getCommands } from '../utils/slash-utilities';
 
 export const VALID_LOCALES = ['pt_br', 'en_us'] as const;
 
 export const LocaleFlags = {
   pt_br: '🇧🇷',
   en_us: '🇺🇸'
-}
+};
 
 export type Locale = typeof VALID_LOCALES[number];
 
@@ -24,7 +26,7 @@ const resources = VALID_LOCALES.reduce((acc, locale) => {
     if (!acc[locale]) {
       acc[locale] = {} as ResourceLanguage;
     }
-    acc[locale][key] = await import(path.join(folderPath, file)).then(m => m.default)
+    acc[locale][key] = await import(path.join(folderPath, file)).then(m => m.default);
   });
 
   logger.debug(`Loaded ${files.length} namespaces for locale ${locale}.`);
@@ -33,15 +35,23 @@ const resources = VALID_LOCALES.reduce((acc, locale) => {
 
 export const init = async () => {
 
+  const commands = getCommands().reduce((acc, command) => {
+    acc[`command_${command.name}`] = command.toString();
+    return acc;
+  }, {} as Record<string, string>);
+
   return i18next.init({
     resources,
     debug: false,
     fallbackLng: DEFAULT_LOCALE,
     supportedLngs: VALID_LOCALES,
-    saveMissing: true
+    saveMissing: true,
+    interpolation: {
+      defaultVariables: commands
+    }
   });
-}
+};
 
 export const isValidLocale = (content: string): content is Locale => {
   return VALID_LOCALES.includes(content as Locale);
-}
+};
