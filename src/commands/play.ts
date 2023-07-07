6d2e9a9140bc1876fa6fa05e0c60162c0f90d6ca
doiská <doiskka@gmail.com>
2023-07-07 17:01:03 -0300

    refactor(commands): updated play and queue command

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,26 +1,31 @@
-import { t } from 'i18next';
-
-import { ApplyOptions } from '@sapphire/decorators';
+import { ActionRowBuilder, APIEmbed, ButtonBuilder, ButtonStyle } from 'discord.js';
+import { Command, container } from '@sapphire/framework';
 import { isGuildMember } from '@sapphire/discord.js-utilities';
-import { Command } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
 
-import { getGuidLocale } from '@/modules/guild-locale';
-import { addNewSong } from '@/music/heizou/add-new-song';
+import { resolveKey } from 'twokei-i18next';
+import { getRandomLoadingMessage } from '@/utils/utils';
+import { Embed } from '@/utils/messages';
 import { getReadableException } from '@/structures/exceptions/utils/get-readable-exception';
-import { sendInteraction, sendLoadingMessage } from '@/utils/utils';
+import { addNewSong } from '@/music/heizou/add-new-song';
+import { playerLogger } from '@/modules/logger-transport';
 
 @ApplyOptions<Command.Options>({
   name: 'play',
   aliases: ['p'],
   description: 'ping pong',
   enabled: true,
+  preconditions: ['GuildTextOnly'],
+  cooldownDelay: 1_000,
 })
 export class PlayCommand extends Command {
   registerApplicationCommands(registry: Command.Registry) {
     registry.registerChatInputCommand((builder) => builder
       .setName(this.name)
       .setDescription(this.description)
-      .addStringOption((option) => option.setName('search').setDescription('Input').setRequired(true)));
+      .addStringOption((option) => option.setName('search')
+        .setDescription('Input')
+        .setRequired(true)));
   }
 
   public override async chatInputRun(
@@ -38,31 +43,72 @@ export class PlayCommand extends Command {
 
     const guildId = interaction.member.guild.id;
 
-    const locale = await getGuidLocale(guildId);
-
-    await sendLoadingMessage(interaction, locale);
+    await container.client.replyTo(
+      interaction,
+      {
+        description: await resolveKey(interaction, getRandomLoadingMessage()) as string,
+      },
+      15,
+    );
 
     try {
       const result = await addNewSong(search, interaction.member);
 
       const [track, ...rest] = result.tracks;
 
-      const trackTranslation = t(
+      const trackTranslation = await resolveKey(interaction,
         rest.length === 0 ? 'song_added' : 'playlist_added',
         {
           track: track.title,
           track_count: rest.length,
           ns: 'player',
-          lng: locale,
-        },
-      );
+          defaultValue: 'Song added: {{track}}',
+        });
 
-      await sendInteraction(interaction, trackTranslation, 15000);
+      playerLogger.info(`${interaction.member.user.tag} added ${track.title} to the queue.`, {
+        guildId,
+        track,
+      });
+
+      const likeButton = new ButtonBuilder()
+        .setCustomId('like')
+        .setLabel('Like')
+        .setStyle(ButtonStyle.Primary);
+
+      const dislikeButton = new ButtonBuilder()
+        .setCustomId('dislike')
+        .setLabel('Dislike')
+        .setStyle(ButtonStyle.Danger);
+
+      const viewOnSource = new ButtonBuilder()
+        .setStyle(ButtonStyle.Link)
+        .setLabel(`View on ${track.sourceName}`)
+        .setURL(track.uri);
+
+      const row = new ActionRowBuilder<ButtonBuilder>();
+      row.addComponents(likeButton, dislikeButton, viewOnSource);
+
+      const responseEmbed = Embed.success(trackTranslation, {
+        author: {
+          name: `Requested by ${interaction.member.user.tag}!`,
+          icon_url: interaction.member.user.displayAvatarURL(),
+        },
+        title: track.title,
+        url: track.uri,
+        thumbnail: {
+          url: track.thumbnail ?? '',
+        },
+        description: '\n\nLike the song to add it to your liked songs!\nOr dislike to not hear it again.',
+      });
+
+      await container.client.replyTo(interaction, {
+        embeds: [responseEmbed as APIEmbed],
+        components: [row],
+      });
     } catch (error) {
-      await sendInteraction(
+      await container.client.replyTo(
         interaction,
-        await getReadableException(error, guildId),
-        15000,
+        Embed.error(await getReadableException(error, interaction.guild)),
       );
     }
   }
---
+++ src/commands/queue.ts
@@ -0,0 +1,62 @@
+import { Command, container } from '@sapphire/framework';
+import { isGuildMember } from '@sapphire/discord.js-utilities';
+import { ApplyOptions } from '@sapphire/decorators';
+
+import { resolveKey } from 'twokei-i18next';
+import { ErrorCodes } from '@/structures/exceptions/ErrorCodes';
+
+@ApplyOptions<Command.Options>({
+  name: 'queue',
+  description: 'View the queue',
+  enabled: true,
+  preconditions: ['GuildOnly'],
+  cooldownDelay: 1_000,
+})
+export class PlayCommand extends Command {
+  public registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand((builder) => builder
+      .setName(this.name)
+      .setDescription(this.description));
+  }
+
+  public override async chatInputRun(
+    interaction: Command.ChatInputCommandInteraction,
+  ) {
+    if (!interaction.guild || !isGuildMember(interaction.member)) {
+      return;
+    }
+
+    const player = container.xiao.getPlayer(interaction.guild);
+
+    if (!player) {
+      return container.client.replyTo(
+        interaction,
+        {
+          description: await resolveKey(
+            interaction.guild,
+            ErrorCodes.NO_PLAYER_FOUND,
+            { ns: 'error', defaultValue: 'No player found' },
+          ) as string,
+        },
+        15,
+      );
+    }
+
+    const { queue } = player;
+
+    const trackList = queue
+      .filter(Boolean)
+      .map(
+        (track, index) => `${index + 1}. [${track?.title}](${queue.current?.uri || ''})`,
+      );
+
+    container.client.replyTo(interaction, {
+      title: queue.current?.title || '',
+      url: queue.current?.uri || '',
+      thumbnail: {
+        url: queue.current?.thumbnail || '',
+      },
+      description: trackList.join('\n'),
+    }, 20);
+  }
+}
