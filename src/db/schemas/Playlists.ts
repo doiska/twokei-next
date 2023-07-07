97b963af9209e625c016ef12271f0004f37b897a
doiská <doiskka@gmail.com>
2023-07-07 17:03:48 -0300

    chore: updated query logger

--- src/db/schemas/Playlists.ts
+++ src/db/schemas/Playlists.ts
@@ -1,11 +1,17 @@
 import { pgSchema, timestamp, varchar } from 'drizzle-orm/pg-core';
 
-export const playlists = pgSchema(process.env.PGSCHEMA ?? 'app').table(
-  'users_playlists',
-  {
-    userId: varchar('user_id').primaryKey().notNull(),
-    playlistName: varchar('playlist_name').notNull(),
-    playlistUrl: varchar('playlist_url').notNull(),
-    createdAt: timestamp('created_at').defaultNow(),
-  },
-);
+export const playlists = pgSchema(process.env.PGSCHEMA ?? 'app')
+  .table(
+    'users_playlists',
+    {
+      userId: varchar('user_id')
+        .primaryKey()
+        .notNull(),
+      playlistName: varchar('playlist_name')
+        .notNull(),
+      playlistUrl: varchar('playlist_url')
+        .notNull(),
+      createdAt: timestamp('created_at')
+        .defaultNow(),
+    },
+  );
