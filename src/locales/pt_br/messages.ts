d3b9f611f6fc0430f396488d1a34dc42f3e3cef3
doiská <doiskka@gmail.com>
2023-07-07 17:04:00 -0300

    feat(i18n): added loading messages

---
+++ src/locales/pt_br/messages.ts
@@ -0,0 +1,12 @@
+import { RandomMessages } from '@/constants/random-messages';
+
+export default {
+  [RandomMessages.LOADING]: 'Carregando...',
+  [RandomMessages.COOKING_UP]: 'Preparando algo especial...',
+  [RandomMessages.JUST_A_SEC]: 'Só um momento...',
+  [RandomMessages.IM_ON_IT]: 'Estou nisso!',
+  [RandomMessages.IM_WORKING_ON_IT]: 'Estou trabalhando nisso! (Não é verdade) (Sou só preguiçoso)',
+  [RandomMessages.HOLD_ON]: 'Aguarde um instante...',
+  [RandomMessages.YOURE_NOT_PAYING_ME_ENOUGH]: 'Você não está me pagando o suficiente para isso...',
+  [RandomMessages.IM_DOING_MY_BEST]: 'Estou fazendo o meu melhor!',
+};
