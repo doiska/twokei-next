ce1bb77dce49137b81ecdd98cebd61d53244a900
doiská <doiskka@gmail.com>
2023-07-22 15:01:53 -0300

    chore: updated drizzleorm/kit versions

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,14 @@
-import type { Config } from 'drizzle-kit';
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({
+  path: './src/.env'
+});
 
 export default {
   schema: './src/db/schemas',
   out: './drizzle',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL,
+  }
 } satisfies Config;
