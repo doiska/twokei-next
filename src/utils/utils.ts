1dd84284157bad30237b04b2c8053612f7615cc5
doiská <doiskka@gmail.com>
2023-08-07 08:43:33 -0300

    refactor(preset-message): added new way to send preset messages

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,14 +5,11 @@ import type {
 } from 'discord.js';
 import { container } from '@sapphire/framework';
 
-import { RandomMessages } from '@/constants/random-messages';
 import { Embed } from '@/utils/messages';
 
-import { resolveKey } from 'twokei-i18next';
+import { resolveKey, type TOptions } from 'twokei-i18next';
 
 export const escapeRegExp = (str: string) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
-type PredefinedMessages = 'loading' | 'success' | 'error';
 
 const embedTypes = {
   success: Embed.success,
@@ -24,8 +21,9 @@ const embedTypes = {
 
 type PresetMessageFn<T = Exclude<RepliableInteraction, ModalSubmitInteraction> | Message> = {
   interaction: T
-  preset: PredefinedMessages
+  preset: keyof typeof embedTypes
   message?: string
+  i18n?: TOptions & { joinArrays?: string | boolean }
   deleteIn?: number
 } & (T extends Message ? ReplyOptions : InteractionReplyOptions);
 
@@ -34,28 +32,41 @@ export async function sendPresetMessage ({
   preset,
   message,
   deleteIn,
+  i18n,
   ...props
 }: PresetMessageFn) {
+  const defaultOptions = message ? { joinArrays: '\n' } : { joinArrays: false, returnObjects: true };
+
+  console.log(defaultOptions);
+
   const found = await resolveKey(
     interaction,
     message ?? `messages:${preset}`,
-    message ? { joinArrays: '\n' } : { returnObjects: true },
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    {
+      ...defaultOptions,
+      ...i18n,
+    },
   );
 
-  const randomMessage = Array.isArray(found) ? found[Math.floor(Math.random() * found.length)] : found;
-  const embed = embedTypes[preset](randomMessage);
+  if (!message) {
+    const randomMessage = Array.isArray(found) ? found[Math.floor(Math.random() * found.length)] : found;
+    const embed = embedTypes[preset](randomMessage);
 
-  await container.reply(interaction, {
+    return await container.reply(interaction, {
+      embeds: [embed],
+      ...props,
+    });
+  }
+
+  const embed = embedTypes[preset ?? 'success'](found);
+
+  return await container.reply(interaction, {
     embeds: [embed],
     ...props,
   }, deleteIn);
 }
-
-// TODO: usar novo meio para gerar as mensagens aleatórias de sucesso e erro
-export const getRandomLoadingMessage = () => {
-  const random = Math.floor(Math.random() * Object.keys(RandomMessages).length);
-  return `messages:${Object.values(RandomMessages)[random]}`;
-};
 
 export type Maybe<T> = T | null | undefined;
 
