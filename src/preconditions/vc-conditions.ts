380f4140d9f73a3dd22affa41588409cf91504f2
doiská <doiskka@gmail.com>
2023-08-07 08:44:05 -0300

    chore: removed unused files

--- src/preconditions/vc-conditions.ts
+++ src/preconditions/vc-conditions.ts
@@ -10,13 +10,13 @@ export const isConnectedTo = (member: GuildMember, channel: Maybe<string>) => {
 export class SameVoiceChannelCondition extends Precondition {
   public override async messageRun (message: Message): Promise<any> {
     if (!message.member?.voice?.channel) {
-      return await this.error({ message: 'You must be connected to a voice channel to use this command.' });
+      return this.error({ message: 'You must be connected to a voice channel to use this command.' });
     }
 
     if (message.guild?.members.me?.voice.channel?.id !== message.member.voice.channel.id) {
-      return await this.error({ message: 'You must be in the same voice channel as me to use this command.' });
+      return this.error({ message: 'You must be in the same voice channel as me to use this command.' });
     }
 
-    return await this.ok();
+    return this.ok();
   }
 }
