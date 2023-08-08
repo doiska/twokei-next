26decaee9f715d77964110f91582880c99c136e3
doiská <doiskka@gmail.com>
2023-08-08 18:07:44 -0300

    Update options.ts

--- src/shoukaku/options.ts
+++ src/shoukaku/options.ts
@@ -3,10 +3,40 @@ import { NodeOption, ShoukakuOptions } from "shoukaku";
 type OptionWithActive = NodeOption & { active?: boolean };
 
 export const Nodes: OptionWithActive[] = [
+  // {
+  //   "name": "Amazon Eduardin",
+  //   "url": "15.228.220.230:2333",
+  //   "auth": "youshallnotpass",
+  //   "secure": false
+  // }
   {
-    "name": "Amazon Eduardin",
-    "url": "15.228.220.230:2333",
-    "auth": "youshallnotpass",
+    "name": "lavalink.lexnet.cc",
+    "url": "lavalink.lexnet.cc:443",
+    "auth": "lexn3tl@val!nk",
+    "secure": true
+  },
+  {
+    "name": "eu-lavalink.lexnet.cc",
+    "url": "eu-lavalink.lexnet.cc:443",
+    "auth": "lexn3tl@val!nk",
+    "secure": true
+  },
+  {
+    "name": "https://horizxon.studio/",
+    "url": "lava1.horizxon.studio:80",
+    "auth": "horizxon.studio",
+    "secure": false
+  },
+  {
+    "name": "https://horizxon.studio/",
+    "url": "lava3.horizxon.studio:80",
+    "auth": "horizxon.studio",
+    "secure": false
+  },
+  {
+    "name": "https://horizxon.studio/",
+    "url": "lava4.horizxon.studio:80",
+    "auth": "horizxon.studio",
     "secure": false
   }
 ];
