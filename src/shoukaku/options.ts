ae0e8c10dfee9b371168d5adfec260cbf72626e8
Pedro H <40711231+doiska@users.noreply.github.com>
2023-08-08 17:59:23 -0300

    Update options.ts

--- src/shoukaku/options.ts
+++ src/shoukaku/options.ts
@@ -8,36 +8,6 @@ export const Nodes: OptionWithActive[] = [
     "url": "15.228.220.230:2333",
     "auth": "youshallnotpass",
     "secure": false
-  },
-  {
-    "name": "lavalink.lexnet.cc",
-    "url": "lavalink.lexnet.cc:443",
-    "auth": "lexn3tl@val!nk",
-    "secure": true
-  },
-  {
-    "name": "eu-lavalink.lexnet.cc",
-    "url": "eu-lavalink.lexnet.cc:443",
-    "auth": "lexn3tl@val!nk",
-    "secure": true
-  },
-  {
-    "name": "https://horizxon.studio/",
-    "url": "lava1.horizxon.studio:80",
-    "auth": "horizxon.studio",
-    "secure": false
-  },
-  {
-    "name": "https://horizxon.studio/",
-    "url": "lava3.horizxon.studio:80",
-    "auth": "horizxon.studio",
-    "secure": false
-  },
-  {
-    "name": "https://horizxon.studio/",
-    "url": "lava4.horizxon.studio:80",
-    "auth": "horizxon.studio",
-    "secure": false
   }
 ];
 
