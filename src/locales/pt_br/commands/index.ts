087d6110fde26d130fb423226d1bb824f454f80c
doiská <doiskka@gmail.com>
2023-08-16 13:28:35 -0300

    feat(news): added news button and text

--- src/locales/pt_br/commands/index.ts
+++ src/locales/pt_br/commands/index.ts
@@ -1,7 +1,9 @@
+import load_playlist from '@/locales/pt_br/commands/load-playlist';
 import queue from './queue';
 import setup from './setup';
 
 export default {
   queue,
   setup,
+  load_playlist,
 };
---
+++ src/locales/pt_br/commands/load-playlist.ts
@@ -0,0 +1,4 @@
+export default {
+  not_found: 'Não encontramos nenhuma playlist em suas fontes, por favor, verifique o link em seu perfil.',
+  too_many_playlists: 'Você possui mais de {{max}} playlists, por isso não podemos exibir todas no menu.',
+};
