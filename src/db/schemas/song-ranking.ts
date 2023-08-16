3d54402889ea0ebf1cb71821c97e01fad69f4a58
doiská <doiskka@gmail.com>
2023-08-16 14:45:42 -0300

    hotfix(songRanking): fixed schema name

--- src/db/schemas/song-ranking.ts
+++ src/db/schemas/song-ranking.ts
@@ -2,7 +2,7 @@ import { eq, sql } from 'drizzle-orm';
 import { pgSchema } from 'drizzle-orm/pg-core';
 import { songUserEvents } from '@/db/schemas/song-user-events';
 
-export const songRanking = pgSchema('dev').view('song_ranking')
+export const songRanking = pgSchema(process.env.PGSCHEMA ?? 'app').view('song_ranking')
   .as(qb => qb.select({
     userId: songUserEvents.userId,
     listened: sql<number>`COUNT(*)`.as('listened'),
