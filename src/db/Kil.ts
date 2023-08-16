5473c3b6e70b71c79bd925ddf1a7302fa1f86991
doiská <doiskka@gmail.com>
2023-08-16 14:24:41 -0300

    chore: updated drizzle-kit

--- src/db/Kil.ts
+++ src/db/Kil.ts
@@ -1,11 +1,15 @@
-import { queryLogger } from '@/modules/logger-transport';
-
 import { drizzle } from 'drizzle-orm/node-postgres';
+
+import { logger, queryLogger } from '@/modules/logger-transport';
+
 import { Pool } from 'pg';
 
 const dbClient = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+logger.info('Connecting to database...');
+logger.info(`Database URL: ${process.env.DATABASE_URL}`);
 
 export const kil = drizzle(dbClient, {
   logger: {
