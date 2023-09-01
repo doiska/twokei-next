6e2264d3911cae593fdefe3a4416bd4ff10ca8a4
doiská <doiskka@gmail.com>
2023-09-01 19:14:21 -0300

    lint: fixing line separators crlf to lf

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -3,7 +3,6 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { isGuildMember } from "@sapphire/discord.js-utilities";
 import { Command } from "@sapphire/framework";
 
-import { setupGuildLanguage } from "@/features/song-channel/setup-guild-language";
 import { setupNewChannel } from "@/features/song-channel/setup-new-channel";
 import { setupSongMessage } from "@/features/song-channel/setup-song-message";
 import { logger } from "@/modules/logger-transport";
@@ -67,7 +66,7 @@ export class PlayCommand extends Command {
       //   logger.info("Error while setupGuildLanguage");
       //   logger.error(e);
       // });
-      
+
       await setupSongMessage(guild, response).catch((e) => {
         logger.info("Error while setupSongMessage");
         logger.error(e);
