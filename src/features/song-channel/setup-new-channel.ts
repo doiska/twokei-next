e9fbd934547f8a1793ee1f3a3b14ebb94dc12cc4
doiská <doiskka@gmail.com>
2023-09-05 10:35:19 -0300

    fix(permissions): removed permission overwrite

--- src/features/song-channel/setup-new-channel.ts
+++ src/features/song-channel/setup-new-channel.ts
@@ -1,9 +1,4 @@
-import {
-  ChannelType,
-  type Guild,
-  PermissionFlagsBits,
-  PermissionsBitField,
-} from "discord.js";
+import { ChannelType, type Guild } from "discord.js";
 import { canSendMessages } from "@sapphire/discord.js-utilities";
 import { container } from "@sapphire/framework";
 import { noop } from "@sapphire/utilities";
@@ -21,22 +16,21 @@ export const setupNewChannel = async (guild: Guild) => {
     );
   }
 
-  const selfPermissions = self.permissions;
-
-  const createChannelPermissions = [
-    PermissionsBitField.Flags.ManageChannels,
-    PermissionsBitField.Flags.ManageMessages,
-    PermissionsBitField.Flags.SendMessages,
-  ];
-
-  const canCreateChannel = createChannelPermissions.every((permission) => {
-    console.log(selfPermissions.has(permission), permission);
-    return selfPermissions.has(permission);
-  });
-
-  if (!canCreateChannel) {
-    throw new FriendlyException("I can't create channels in this server.");
-  }
+  // const selfPermissions = self.permissions;
+  //
+  // const createChannelPermissions = [
+  //   PermissionsBitField.Flags.ManageChannels,
+  //   PermissionsBitField.Flags.ManageMessages,
+  //   PermissionsBitField.Flags.SendMessages,
+  // ];
+  //
+  // const canCreateChannel = createChannelPermissions.every((permission) => {
+  //   return selfPermissions.has(permission);
+  // });
+  //
+  // if (!canCreateChannel) {
+  //   throw new FriendlyException("I can't create channels in this server.");
+  // }
 
   const currentChannel = await container.sc.get(guild);
 
@@ -56,20 +50,18 @@ export const setupNewChannel = async (guild: Guild) => {
     guild: { name: guild.name, id: guild.id },
   });
 
-  const newChannel = await guild.channels.create({
-    name: "twokei-music",
-    type: ChannelType.GuildText,
-    permissionOverwrites: [
-      {
-        id: Twokei.user.id,
-        allow: [
-          PermissionFlagsBits.ManageChannels,
-          PermissionFlagsBits.ManageMessages,
-          PermissionFlagsBits.SendMessages,
-        ],
-      },
-    ],
-  });
+  const newChannel = await guild.channels
+    .create({
+      name: "twokei-music",
+      type: ChannelType.GuildText,
+    })
+    .catch((e) => {
+      logger.error(e);
+    });
+
+  if (!newChannel) {
+    throw new FriendlyException("I can't create a new channel.");
+  }
 
   if (!canSendMessages(newChannel)) {
     throw new FriendlyException("I can't send messages in the new channel.");
