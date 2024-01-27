ef2f0fbd4aa8605952a240a44ef58b250e60300a
doiská <doiskka@gmail.com>
2024-01-27 12:51:29 -0300

    refactor(ranking): new ranking view and analytics-track-info

--- src/db/schemas/analytics-track-info.ts
+++ src/db/schemas/analytics-track-info.ts
@@ -2,17 +2,26 @@ import { createTable } from "@/db/Kil";
 import {
   integer,
   jsonb,
+  pgMaterializedView,
+  pgSchema,
   timestamp,
   unique,
   varchar,
 } from "drizzle-orm/pg-core";
+import { env } from "@/app/env";
+import { eq, sql } from "drizzle-orm";
+
+const view =
+  env.PG_SCHEMA === "public"
+    ? pgMaterializedView
+    : pgSchema(env.PG_SCHEMA).materializedView;
 
 export const analyticsTrackInfo = createTable("analytics_track_info", {
   id: varchar("id").notNull().primaryKey(),
-  spotify_id: varchar("spotify_id").notNull(),
-  title: varchar("title").notNull(),
-  author: varchar("author"),
-  duration: integer("duration"),
+  spotify_id: varchar("spotify_id"),
+  title: varchar("title"),
+  artists: varchar("artists"),
+  durationInMs: integer("duration_in_ms"),
 });
 
 export const analyticsUserListenedTracks = createTable(
@@ -46,3 +55,25 @@ export const analyticsUserAddedTracks = createTable(
     composite: unique().on(table.userId, table.trackId, table.guildId),
   }),
 );
+
+export const listeningRanking = pgMaterializedView("ranking").as((qb) => {
+  return qb
+    .select({
+      userId: analyticsUserListenedTracks.userId,
+      listenedInMs:
+        sql<number>`SUM(${analyticsTrackInfo.durationInMs} * ${analyticsUserListenedTracks.listened})`.as(
+          "listened_in_ms",
+        ),
+      position:
+        sql<number>`ROW_NUMBER() OVER (ORDER BY SUM(${analyticsTrackInfo.durationInMs} * ${analyticsUserListenedTracks.listened}) DESC)`.as(
+          "position",
+        ),
+    })
+    .from(analyticsUserListenedTracks)
+    .innerJoin(
+      analyticsTrackInfo,
+      eq(analyticsUserListenedTracks.trackId, analyticsTrackInfo.id),
+    )
+    .groupBy(analyticsUserListenedTracks.userId)
+    .orderBy(sql`listened_in_ms DESC`);
+});
--- src/lib/analytics/track-event.ts
+++ src/lib/analytics/track-event.ts
@@ -1,65 +1,85 @@
 import {
+  analyticsTrackInfo,
   analyticsUserAddedTracks,
   analyticsUserListenedTracks,
 } from "@/db/schemas/analytics-track-info";
 import { kil } from "@/db/Kil";
 import { sql } from "drizzle-orm";
+import { ResolvableTrack } from "@/music/structures/ResolvableTrack";
 
 interface SongEvent {
   users: string[];
   event: "liked_song" | "disliked_song" | "added_song" | "heard_song";
   guild?: string;
-  track: {
-    isrc: string;
-  };
+  track: ResolvableTrack;
 }
 
-export async function trackEvent(event: SongEvent) {
-  if (event.event === "heard_song") {
-    await kil
-      .insert(analyticsUserListenedTracks)
-      .values(
-        event.users.map((user) => ({
-          userId: user,
-          trackId: event.track.isrc,
-          guildId: event.guild,
-          listened: 1,
-        })),
-      )
-      .onConflictDoUpdate({
-        set: {
-          listened: sql`${analyticsUserListenedTracks.listened} + 1`,
-        },
-        target: [
-          analyticsUserListenedTracks.userId,
-          analyticsUserListenedTracks.trackId,
-          analyticsUserListenedTracks.guildId,
-        ],
-      });
+async function trackAddedSong(event: SongEvent) {
+  await kil
+    .insert(analyticsUserAddedTracks)
+    .values(
+      event.users.map((user) => ({
+        userId: user,
+        trackId: event.track.isrc!,
+        guildId: event.guild,
+        listened: 1,
+      })),
+    )
+    .onConflictDoUpdate({
+      set: {
+        listened: sql`${analyticsUserAddedTracks.listened} + 1`,
+      },
+      target: [
+        analyticsUserAddedTracks.userId,
+        analyticsUserAddedTracks.trackId,
+        analyticsUserAddedTracks.guildId,
+      ],
+    });
+}
 
+async function trackHeardSong(event: SongEvent) {
+  await kil
+    .insert(analyticsUserListenedTracks)
+    .values(
+      event.users.map((user) => ({
+        userId: user,
+        trackId: event.track.isrc!,
+        guildId: event.guild,
+        listened: 1,
+      })),
+    )
+    .onConflictDoUpdate({
+      set: {
+        listened: sql`${analyticsUserListenedTracks.listened} + 1`,
+      },
+      target: [
+        analyticsUserListenedTracks.userId,
+        analyticsUserListenedTracks.trackId,
+        analyticsUserListenedTracks.guildId,
+      ],
+    });
+}
+
+const track = {
+  added_song: trackAddedSong,
+  heard_song: trackHeardSong,
+} as Record<SongEvent["event"], (event: SongEvent) => Promise<void>>;
+
+export async function trackEvent(event: SongEvent) {
+  if (!(event.event in track) || !event.track.isrc) {
     return;
   }
 
-  if (event.event === "added_song") {
-    await kil
-      .insert(analyticsUserAddedTracks)
-      .values(
-        event.users.map((user) => ({
-          userId: user,
-          trackId: event.track.isrc,
-          guildId: event.guild,
-          listened: 1,
-        })),
-      )
-      .onConflictDoUpdate({
-        set: {
-          listened: sql`${analyticsUserAddedTracks.listened} + 1`,
-        },
-        target: [
-          analyticsUserAddedTracks.userId,
-          analyticsUserAddedTracks.trackId,
-          analyticsUserAddedTracks.guildId,
-        ],
-      });
-  }
+  await kil
+    .insert(analyticsTrackInfo)
+    .values({
+      id: event.track.isrc,
+      spotify_id: event.track.identifier,
+      title: event.track.title,
+      artists: event.track.author,
+      durationInMs: event.track.length,
+    })
+    .onConflictDoNothing();
+
+  await track[event.event](event);
 }
