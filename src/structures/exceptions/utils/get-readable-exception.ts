23f96e3cfaf3525548c80057a57a4298be7902f3
doiska <doiskka@gmail.com>
2024-03-30 19:09:11 -0300

    fix: hardcoded translation

--- src/structures/exceptions/utils/get-readable-exception.ts
+++ src/structures/exceptions/utils/get-readable-exception.ts
@@ -13,5 +13,5 @@ export const getReadableException = (error: unknown) => {
   }
 
   logger.error(error);
-  return "An unexpected error occurred, please try again.";
+  return "Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.";
 };
