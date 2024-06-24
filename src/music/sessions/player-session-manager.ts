41dcedfbe96a88a753dbfab4be8b54e87539a231
doiska <doiska@proton.me>
2024-06-24 19:28:06 -0300

    refactor(session): player session duration

--- src/music/sessions/player-session-manager.ts
+++ src/music/sessions/player-session-manager.ts
@@ -5,20 +5,24 @@ import { container } from "@sapphire/framework";
 
 export async function restoreExpiredSessions(sessions: PlayerSession[]) {
   for (const session of sessions) {
-    logger.debug(`Cleaning up session ${session.guildId}.`);
-    const guild = await container.client.guilds.fetch(session.guildId);
+    try {
+      logger.debug(`Cleaning up session ${session.guildId}.`);
+      const guild = await container.client.guilds.fetch(session.guildId);
 
-    if (!guild) {
-      continue;
+      if (!guild) {
+        continue;
+      }
+
+      logger.debug(`Cleaned session ${session.guildId}.`);
+      await container.sc.reset(guild);
+    } catch (e) {
+      logger.error(`Failed to clean up session ${session.guildId}.`, e);
     }
-
-    logger.debug(`Cleaned session ${session.guildId}.`);
-    await container.sc.reset(guild);
   }
 }
 
 export async function getSessions() {
-  const allSessions = (await kil.select().from(playerSessions)) ?? [];
+  const allSessions = await kil.select().from(playerSessions);
 
   const isExpired = (session: PlayerSession) => {
     const isQueueValid =
@@ -30,24 +34,19 @@ export async function getSessions() {
     return isSessionExpired || !isQueueValid;
   };
 
-  const validSessions = allSessions.filter((session) => !isExpired(session));
-  const expiredSessions = allSessions.filter((session) => isExpired(session));
+  const dumpState = [];
+  const validSessions = [];
+  const expiredSessions = [];
 
-  const dumpState = validSessions.map((session) => {
-    const now = Date.now();
-    const lastUpdate = session.state.timestamp;
+  for (const session of allSessions) {
+    if (isExpired(session)) {
+      expiredSessions.push(session);
+      continue;
+    }
 
-    const duration = session.state.player.position ?? 0;
-
-    const correction = now - lastUpdate + 5000;
-
-    logger.debug(
-      `Session ${session.guildId} was last updated ${lastUpdate}ms ago. Correcting by ${correction}ms.`,
-    );
-
-    session.state.player.position = duration + correction;
-    return [session.guildId, session.state];
-  });
+    validSessions.push(session);
+    dumpState.push([session.guildId, session.state]);
+  }
 
   return {
     valid: validSessions,
